b06ea39ba43b300b23423ce400b3e176542cd762
Cole Robinson <CWRobinson@gmx.com>
2021-04-13 16:45:41 -0500

    event listener to update new chats
    Lines 5-11 for an event listener that catches new messages

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,18 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newMessage = document.querySelector('.new-chat');
+
+newMessage.addEventListener('submit', e=>{
+    e.preventDefault();
+    const message = newMessage.message.value.trim();
+    chatroom.addChat(message)
+        .then(()=> newChatForm.reset())
+        .catch(err => console.log(err));
+});
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'shaun');
+const chatroom = new Chatroom('gaming', 'cole');
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
