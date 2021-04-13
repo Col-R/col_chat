886d9007ccf106a73cb4250f416ed34300201945
Cole Robinson <CWRobinson@gmx.com>
2021-04-12 19:33:28 -0500

    callback for chat snapshot + tests

--- scripts/chat.js
+++ scripts/chat.js
@@ -7,7 +7,8 @@ class Chatroom {
     constructor(channel, username){
         this.channel = channel;
         this.username = username;
-        this.chats = db.collection('chats')
+        this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat (message){
         // format a chat object
@@ -22,10 +23,27 @@ class Chatroom {
         const response = await this.chats.add(chat);
         return response;
     }
+    getChats(callback){
+        this.unsub = this.chats
+            .where('channel', '==', this.channel)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added'){
+                        //update UI
+                        callback(change.doc.data());
+                    }
+                });
+            });
+    }
+    updateName(username){
+        this.username = username;
+    }
+    updateChannel(channel){
+        this.channel = channel;
+        console.log('channel changed');
+        if (this.unsub){
+            this.unsub();
+        }
+    }
 }
-
-const chatroom = new Chatroom('gaming', 'col');
-
-chatroom.addChat('hello everyone')
-    .then(()=> console.log('chat added'))
-    .catch(err =>console.log(err));
