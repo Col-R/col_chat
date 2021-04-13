cfe102b9184fd8402259eb43e0a443425c030d33
Cole Robinson <CWRobinson@gmx.com>
2021-04-13 17:25:03 -0500

    username storage
    added local storage function to store past chosen usernames for next time. Otherwise the name will be 'anonymous'

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,33 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newMessage = document.querySelector('.new-chat');
-
+const newUsername = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg')
+//messages
 newMessage.addEventListener('submit', e=>{
     e.preventDefault();
     const message = newMessage.message.value.trim();
     chatroom.addChat(message)
-        .then(()=> newChatForm.reset())
+        .then(()=> newMessage.reset())
         .catch(err => console.log(err));
 });
 
+// username
+newUsername.addEventListener('submit', e =>{
+    e.preventDefault();
+    const newUser = newUsername.name.value.trim();
+    chatroom.updateName(newUser)
+    newUsername.reset();
+    updateMsg.innerText = `Your name was changed to ${newUser}`;
+    setTimeout(()=> updateMsg.innerText = '', 3000);
+});
+
+// check local storage for previous username
+const username = localStorage.username ? localStorage.username : 'anonymous';
+
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', 'cole');
+const chatroom = new Chatroom('gaming', username);
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
