95cc9dc83149feee6b6fde68121afab8ede0484e
Cole Robinson <CWRobinson@gmx.com>
2021-04-15 17:31:03 -0500

    switching between channels
    Added lines 27-35 to swap between different text channels.
    Also a small change that makes general the default channel on load

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const chatList = document.querySelector('.chat-list');
 const newMessage = document.querySelector('.new-chat');
 const newUsername = document.querySelector('.new-name');
 const updateMsg = document.querySelector('.update-msg')
+const channel = document.querySelector ('.chat-rooms');
+
 //messages
 newMessage.addEventListener('submit', e=>{
     e.preventDefault();
@@ -22,12 +24,22 @@ newUsername.addEventListener('submit', e =>{
     setTimeout(()=> updateMsg.innerText = '', 3000);
 });
 
+//update the text channel
+channel.addEventListener('click', e=>{
+    // e.preventDefault;
+    if(e.target.tagName === 'BUTTON'){
+        chatUI.clear();
+        chatroom.updateChannel(e.target.getAttribute('id'));
+        chatroom.getChats(chat=>chatUI.render(chat))
+    }
+});
+
 // check local storage for previous username
 const username = localStorage.username ? localStorage.username : 'anonymous';
 
 // class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('gaming', username);
+const chatroom = new Chatroom('general', username);
 
 // get chats & render
 chatroom.getChats(data => chatUI.render(data));
